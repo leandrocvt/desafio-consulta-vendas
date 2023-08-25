29f758bca9f122310c50508002c8bf14fec7090d
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-08-25 17:38:52 -0300

    feature: sales report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,18 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT obj " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Sale> searchSalesByParams(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,20 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.devsuperior.dsmeta.dto.SaleDTO;
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -20,4 +27,18 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SaleDTO> getReport(String minDateStr, String maxDateStr, String name, Pageable pageable) {
+		LocalDate minDate = minDateStr.isEmpty() ? parseMaxDate(maxDateStr)
+				.minusYears(1L) : LocalDate.parse(minDateStr);
+		LocalDate maxDate = maxDateStr.isEmpty() ? LocalDate.now() : LocalDate.parse(maxDateStr);
+
+		Page<Sale> result = repository.searchSalesByParams(minDate, maxDate, name, pageable);
+		return result.map(SaleDTO::new);
+	}
+
+	private LocalDate parseMaxDate(String maxDateStr) {
+		return maxDateStr.isEmpty() ? LocalDate.now() : LocalDate.parse(maxDateStr);
+	}
+
 }
